22c632fbc6aeeaafe3514296f59b1ffbbbe1f289
silyones <trishasilver12@gmail.com>
2026-08-16 15:42:16 +0530

    exploration of Q

--- src/analysis.py
+++ src/analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("data/raw/higgs_4lepton.csv")
+'''
 #print(df)
 print("first few rows of dataset:")
 print(df.head())
@@ -11,3 +12,8 @@ print(df.shape)
 
 print("\ncolumns in dataset")
 print(df.columns.tolist())
+'''
+
+#to print charge of the 4 leptons 
+print("\ncharge of the 4 leptons:")
+print(df[["Event","Q1","Q2","Q3","Q4"]])
